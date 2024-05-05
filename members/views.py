6fce56afa3a9f2bef500c13245d64059dda37b1a
谢嘉伟 <asdfg756868947@126.com>
2024-05-05 22:40:07 +0800

    init

---
+++ members/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def membersbk(request):
+    return HttpResponse("Hello world!")
+
+
+def members(request):
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
