26c9307a761414f403bd25e547541921c2e7064e
谢嘉伟 <asdfg756868947@126.com>
2024-05-05 23:03:55 +0800

    ADD bootstrap 5

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-xyvq@*tq&c69fxvpvbag3q@vli%4dzsuivuqso_l7ye-1cth-l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
